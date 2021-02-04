125151a3616034a8f82e422606c285a54e2a7ffc
IShungite <ishungite@gmail.com>
2021-02-04 11:45:02 +0100

    add index.php

---
+++ projet/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Infra</title>
+</head>
+<body>
+    <h1>Projet Infra</h1>
+    <?php 
+    phpinfo();
+?>
+</body>
+</html>
+
