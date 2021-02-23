94ee7581c2cc934175933585109f84ccd7acf3dd
IShungite <ishungite@gmail.com>
2021-02-23 11:05:28 +0100

    update

--- projet/index.php
+++ projet/index.php
@@ -1,30 +1,26 @@
-<<<<<<< HEAD
-<html>
-        <head>
-                <title>Test</title>
-        </head>
-        <body>
-              <h1>Test</h1>
-              <?php
-               $output = shell_exec('./etest.sh');
-               echo "<pre>$output</pre>";
-              ?>
-        </body>
-</html>
-=======
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet Infra</title>
+    <title>Projet infra</title>
 </head>
-<body>
-    <h1>Projet Infra</h1>
-    <?php 
-    phpinfo();
-?>
-</body>
-</html>
 
->>>>>>> 9458de1a177aa89f4e3374a76ced67239cb07877
+<body>
+    <h1>Projet infra</h1>
+    <h2>Server List</h2>
+
+    <?php
+    foreach (glob("../scripts/config/*") as $folderPath) {
+        $folderName = basename($folderPath);
+        echo "<a href='serverBoard.php?server=${folderName}'>- ${folderName}</a>";
+    }
+    ?>
+    <br>
+    <br>
+    <a href="createServer.php">Create new server</a>
+</body>
+
+</html>
