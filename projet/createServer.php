598b1e704fb5e924472e65862bf98df6f1402094
IShungite <ishungite@gmail.com>
2021-03-15 16:31:14 +0100

    Add header/footer

--- projet/createServer.php
+++ projet/createServer.php
@@ -1,50 +1,35 @@
-<?php
-include 'config.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once 'header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<h1>Create server</h1>
+<div id="form_page">
+    <form name='form1' method="post" action="scripts/script_createServer.php">
+        <div id="form_body">
+            <?php
+            $jsondata = file_get_contents($TEMPLATE_CONFIG_PATH);
+            // Convert to array 
+            $array = json_decode($jsondata, true);
 
-<body>
-    <h1>Create server</h1>
-
-    <div id="form_page">
-        <form name='form1' method="post" action="scripts/script_createServer.php">
-            <div id="form_body">
-                <?php
-                $jsondata = file_get_contents($TEMPLATE_CONFIG_PATH);
-                // Convert to array 
-                $array = json_decode($jsondata, true);
-
-                foreach ($array as $key => $value) {
-                    if (is_array($value)) {
-                        foreach ($value as $key2 => $value2) {
-                            echo "<div id='$key2' class='input'>$key2
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    foreach ($value as $key2 => $value2) {
+                        echo "<div id='$key2' class='input'>$key2
                                 <input id='$key2' value='$value2' type='text'
                                     Placeholder='$key2' name='$key2'required> 
                                 </div>";
-                        }
-                    } else {
-                        echo "<div id='$key' class='input'>$key
+                    }
+                } else {
+                    echo "<div id='$key' class='input'>$key
                             <input id='$key' value='$value' type='text'
                                 Placeholder='$key' name='$key'required> 
                             </div>";
-                    }
                 }
-                ?>
+            }
+            ?>
 
-                <div class="id input">
-                    <input id="submit" type="submit" name="submit" value="submit" onclick="on_submit()">
-                </div>
+            <div class="id input">
+                <input id="submit" type="submit" name="submit" value="submit" onclick="on_submit()">
             </div>
-        </form>
-    </div>
-</body>
-
-</html>
+        </div>
+    </form>
+</div>
+<?php require_once 'footer.php'; ?>
--- projet/index.php
+++ projet/index.php
@@ -1,26 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once 'header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet infra</title>
-</head>
+<h1>Projet infra</h1>
+<h2>Server List</h2>
 
-<body>
-    <h1>Projet infra</h1>
-    <h2>Server List</h2>
+<?php
+foreach (glob("../scripts/config/*") as $folderPath) {
+    $folderName = basename($folderPath);
+    echo "<a href='serverBoard.php?server=${folderName}'>- ${folderName}</a> </br>";
+}
+?>
+<br>
+<br>
+<a href="createServer.php">Create new server</a>
 
-    <?php
-    foreach (glob("../scripts/config/*") as $folderPath) {
-        $folderName = basename($folderPath);
-        echo "<a href='serverBoard.php?server=${folderName}'>- ${folderName}</a> </br>";
-    }
-    ?>
-    <br>
-    <br>
-    <a href="createServer.php">Create new server</a>
-</body>
-
-</html>
+<?php require_once 'footer.php'; ?>
