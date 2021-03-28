35b8dbe8b3e173d1860a32a85b1c4e2f4f3da2d1
Brololo <laurent-mrl@outlook.fr>
2021-03-28 18:16:46 +0200

    add start server and fix create server

---
+++ scripts/start_server.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+load_json() {
+    configFile="config/${serverPrefix}/config.json"
+    if [ ! -e ${configFile} ] ; then
+        echo "config file doesn't exist"
+        exit 1
+    fi
+    srvChannelNumber=$(jq '.server_channelNumber' < ${configFile} | sed 's/"//g')
+    srvCoreNumber=$(jq '.server_coreNumber' < ${configFile} | sed 's/"//g')
+}
+
+startDb(){
+    echo "start Db"
+    cd "${dest}/db" && "./${serverPrefix}-db" >> syserr_start 2>> syslog_start & sleep 0.1
+    sleep 5
+    echo "Db Complete"
+}
+
+startAuth(){
+    echo "start Auth"
+    cd "${dest}/auth" && "./${serverPrefix}-auth" >> syserr_start 2>> syslog_start & sleep 0.1
+    echo "Auth Complete"
+}
+
+startChan(){
+    echo "startChan"
+    local chanPath="${dest}/chan"
+    for (( i=1; i<=${srvChannelNumber}; i++ )); do
+        for (( j=1; j<=${srvCoreNumber}; j++ )); do
+            cd "${chanPath}/ch${i}/core${j}" && "./${serverPrefix}-ch${i}-core${j}" >> syserr_start 2>> syslog_start & sleep 0.1
+        done
+    done
+    cd "${chanPath}/ch99/core99" && "./${serverPrefix}-ch99-core99" >> syserr_start 2>> syslog_start & sleep 0.1
+    echo "startChan Complete"
+}
+
+main(){
+    serverPrefix=${1}
+    load_json
+    dest="/home/server/${serverPrefix}"
+    mkdir -p ${dest}
+    startDb
+    startAuth
+    startChan
+}
+
+main ${1}
