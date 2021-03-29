901a8a54a739c8d141034be18273d565e9a0078d
Brololo <59967750+Brololo@users.noreply.github.com>
2021-03-29 22:31:59 +0200

    Create stop_server.sh

---
+++ scripts/stop_server.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+load_json() {
+    configFile="config/${serverPrefix}/config.json"
+    if [ ! -e ${configFile} ] ; then
+        echo "config file doesn't exist"
+        exit 1
+    fi
+    srvChannelNumber=$(jq '.server_channelNumber' < ${configFile} | sed 's/"//g')
+    srvCoreNumber=$(jq '.server_coreNumber' < ${configFile} | sed 's/"//g')
+}
+
+stopDb(){
+    echo "stopDb"
+    local PID_PATH="${dest}/db/pid"
+    if [ -e ${PID_PATH} ]
+    then
+        local PID_READ=`cat ${PID_PATH}`
+        if ps -p ${PID_READ} > /dev/null
+        then
+            echo "Killing PID : ${PID_READ}"
+            kill -9 ${PID_READ} > /dev/null 2> /dev/null & sleep 0.1
+        else
+            echo "PID ${PID_READ} NOT FOUND"
+        fi
+    else
+        echo "FILE ${PID_PATH} NOT FOUND"
+    fi
+    echo "stopDb Complete"
+}
+
+stopAuth(){
+    echo "stopAuth"
+    local PID_PATH="${dest}/auth/pid"
+    if [ -e ${PID_PATH} ]
+    then
+        local PID_READ=`cat ${PID_PATH}`
+        if ps -p ${PID_READ} > /dev/null
+        then
+            echo "Killing PID : ${PID_READ}"
+            kill -9 ${PID_READ} > /dev/null 2> /dev/null & sleep 0.1
+        else
+            echo "PID ${PID_READ} NOT FOUND"
+        fi
+    else
+        echo "FILE ${PID_PATH} NOT FOUND"
+    fi
+    echo "stopAuth Complete"
+}
+
+stopChan(){
+    echo "stopChan"
+    local chanPath="${dest}/chan"
+    for (( i=1; i<=${srvChannelNumber}; i++ )); do
+        for (( j=1; j<=${srvCoreNumber}; j++ )); do
+            local PID_PATH="${chanPath}/ch${i}/core${j}/pid"
+            if [ -e ${PID_PATH} ]
+            then
+                local PID_READ=`cat ${PID_PATH}`
+                if ps -p ${PID_READ} > /dev/null
+                then
+                    echo "Killing PID : ${PID_READ}"
+                    kill -9 ${PID_READ} > /dev/null 2> /dev/null & sleep 0.1
+                else
+                    echo "PID ${PID_READ} NOT FOUND"
+                fi
+            else
+                echo "FILE ${PID_PATH} NOT FOUND"
+            fi
+        done
+    done
+    local PID_PAT99H="${chanPath}/ch99/core99/pid"    
+    if [ -e ${PID_PAT99H} ]
+            then
+                local PID_READ99=`cat ${PID_PAT99H}`
+                if ps -p ${PID_READ99} > /dev/null
+                then
+                    echo "Killing PID : ${PID_READ99}"
+                    kill -9 ${PID_READ99} > /dev/null 2> /dev/null & sleep 0.1
+                else
+                    echo "PID ${PID_READ99} NOT FOUND"
+                fi
+            else
+                echo "FILE ${PID_PAT99H} NOT FOUND"
+            fi
+    echo "stopChan Complete"
+}
+
+main(){
+    serverPrefix=${1}
+    load_json
+    dest="/home/server/${serverPrefix}"
+    stopDb
+    stopAuth
+    stopChan
+}
+
+main ${1}
